e66c5f2c7d716c192fe12f5620b44bd3d0963c07
Lingy94 <j.lingyx@gmail.com>
2018-12-23 12:37:45 +0000

    Wrote methods for controller and view pages.

--- medicly/app/controllers/appointments_controller.rb
+++ medicly/app/controllers/appointments_controller.rb
@@ -1,2 +1,52 @@
 class AppointmentsController < ApplicationController
+  before_action :find_appointment, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @appointments = Appointment.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+      @appointment = Appointment.new(appointment_params)
+      if @appointment.valid?
+        @appointment.save
+        redirect_to appointment_path(@appointment)
+      else
+        flash[:errors] = @appointment.errors.full_messages
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @appointment.valid?
+        @appointment.save
+        redirect_to appointment_path(@appointment)
+      else
+        flash[:errors] = @appointment.errors.full_messages
+        render :new
+      end
+    end
+
+    def destroy
+      @appointment.destroy
+      redirect_to appointments_path
+    end
+
+  private
+
+    def find_appointment
+      @appointment = Appointment.find(params[:id])
+    end
+
+    def appointment_params
+      params.require(:appointment).permit(:doctor_id, :patient_id, :appointment_type_id, :note)
+    end
 end
--- medicly/app/controllers/doctors_controller.rb
+++ medicly/app/controllers/doctors_controller.rb
@@ -2,24 +2,43 @@ class DoctorsController < ApplicationController
   before_action :find_doctor, only: [:show, :edit, :update, :destroy]
 
     def index
+      @doctors = Doctor.all
     end
 
     def show
     end
 
     def new
+      @doctor = Doctor.new
     end
 
     def create
+      @doctor = Doctor.new(doctor_params)
+      if @doctor.valid?
+        @doctor.save
+        redirect_to doctor_path(@doctor)
+      else
+        flash[:errors] = @doctor.errors.full_messages
+        render :new
+      end
     end
 
     def edit
     end
 
     def update
+      if @doctor.valid?
+        @doctor.save
+        redirect_to doctor_path(@doctor)
+      else
+        flash[:errors] = @doctor.errors.full_messages
+        render :new
+      end
     end
 
     def destroy
+      @doctor.destroy
+      redirect_to doctors_path
     end
 
   private
