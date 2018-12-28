35576fdc9a5676a9930c03db398cced56cbcd3ee
Kevmsutton <kevmsutton@gmail.com>
2018-12-28 12:02:50 +0000

    added patients controller

--- medicly/app/controllers/patients_controller.rb
+++ medicly/app/controllers/patients_controller.rb
@@ -8,24 +8,42 @@ before_action :find_patient, only: [:show, :edit, :update, :destroy]
   end
 
   def new
+    @patient = Patient.new
   end
 
   def create
+    @patient = Patient.new(patient_params)
+    if @patient.valid?
+      @patient.save
+      redirect_to patient_path(@patient)
+    else
+      flash[:errors] = @patient.errors.full_messages
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @patient.valid?
+      @patient.update(patient_params)
+      redirect_to patient_path(@patient)
+    else
+      flash[:errors] = @patient.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
+    @patient.destroy
+    redirect_to patient_path
   end
 
 private
 
   def find_patient
-    @patient = Patient.find(params[:id])
+    @patient = Patient.all.find(params[:id])
   end
 
   def patient_params
