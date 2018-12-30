84dd9a3eaeb5c22987a7236643936477f4c11f26
ja9-look <janineluk95@gmail.com>
2018-12-30 16:56:52 +0000

    added seed data for patients, doctors, and appointment types

--- medicly/db/seeds.rb
+++ medicly/db/seeds.rb
@@ -25,30 +25,29 @@ doctors = [
 doctors.each {|doctor| Doctor. create(doctor)}
 
 patients = [
-  {name: 'Samantha', age: 23, birth_date: 19950822, gender: 'female', allergies: 'shellfish'},
-  {name: 'Louis', age: 16, birth_date: 20021002, gender: 'male', allergies: 'ibuprofen'},
-  {name: 'Ronald', age: 35, birth_date: 19830217, gender: 'male', allergies: 'nuts'},
-  {name: 'Alessandra', age: 54, birth_date: 19641226, gender: 'female', allergies: 'none'},
-  {name: 'Peter', age: 80, birth_date: 19380402, gender: 'male', allergies: 'kiwi, pineapple'},
-  {name: 'Gisele', age: 73, birth_date: 19451115, gender: 'female', allergies: 'gluten'},
-  {name: 'Georgia', age: 43, birth_date: 1975-07-29, gender: 'female', allergies: 'none'},
-  {name: 'Nadine', age: 15, birth_date: 2003-05-16, gender: 'female', allergies: 'nuts'},
-  {name: 'Chris', age: 36, birth_date: 1982-09-08, gender: 'male', allergies: 'none'}
+  {name: 'Louis Jackson', age: 16, birth_date: "2002-10-02", gender: 'male', allergies: 'ibuprofen'},
+  {name: 'Ronald Dawson', age: 35, birth_date: "1983-02-17", gender: 'male', allergies: 'nuts'},
+  {name: 'Alessandra Lake', age: 54, birth_date: "1964-12-26", gender: 'female', allergies: 'none'},
+  {name: 'Peter Lasalle', age: 80, birth_date: "1938-04-02", gender: 'male', allergies: 'kiwi, pineapple'},
+  {name: 'Gisele Peters', age: 73, birth_date: "1945-11-15", gender: 'female', allergies: 'gluten'},
+  {name: 'Georgia Smith', age: 43, birth_date: "1975-07-29", gender: 'female', allergies: 'none'},
+  {name: 'Nadine Knight', age: 15, birth_date: "2003-05-16", gender: 'female', allergies: 'nuts'},
+  {name: 'Chris Donald', age: 36, birth_date: "1982-09-08", gender: 'male', allergies: 'none'}
 ]
 
 patients.each {|patient| Patient. create(patient)}
 
 appointments = [
-  {doctor_id: 2, patient_id: 3, appointment_type_id: 1, note: 'Diagnosis: Mild Flu and Chest Infection'},
-  {doctor_id: 4, patient_id: 7, appointment_type_id: 2, note: 'Vaccine: MMR'},
-  {doctor_id: 6, patient_id: 3, appointment_type_id: 1, note: 'Diagnosis: Period cramp and pain in lower back'},
-  {doctor_id: 3, patient_id: 5, appointment_type_id: 3, note: 'Emergency: Displaced shoulder'},
-  {doctor_id: 6, patient_id: 1, appointment_type_id: 1, note: 'Diagnosis: Twisted ankle'},
-  {doctor_id: 4, patient_id: 8, appointment_type_id: 3, note: 'Emergency: Asthma attack due to nut allergic reaction'},
-  {doctor_id: 5, patient_id: 5, appointment_type_id: 1, note: 'Diagnosis: Mild UTI'},
-  {doctor_id: 1, patient_id: 4, appointment_type_id: 2, note: 'Vaccine: Chickenpox'},
-  {doctor_id: 9, patient_id: 3, appointment_type_id: 2, note: 'Vaccine: Flu'},
-  {doctor_id: 3, patient_id: 2, appointment_type_id: 1, note: 'Diagnosis: Nausea and dizziness due to low blood pressure'}
+  {doctor_id: 2, patient_id: 3, appointment_type_id: 1, note: "Diagnosis: Mild Flu and Chest Infection", appointment_date: "2018-10-22"},
+  {doctor_id: 4, patient_id: 7, appointment_type_id: 2, note: "Vaccine: MMR", appointment_date: "2017-09-18"},
+  {doctor_id: 6, patient_id: 3, appointment_type_id: 1, note: "Diagnosis: Period cramp and pain in lower back", appointment_date: "2018-03-05"},
+  {doctor_id: 3, patient_id: 5, appointment_type_id: 3, note: "Emergency: Displaced shoulder", appointment_date: "2014-11-28"},
+  {doctor_id: 6, patient_id: 1, appointment_type_id: 1, note: "Diagnosis: Twisted ankle", appointment_date: "2017-08-09"},
+  {doctor_id: 4, patient_id: 8, appointment_type_id: 3, note: "Emergency: Asthma attack due to nut allergic reaction", appointment_date: "2016-04-18"},
+  {doctor_id: 5, patient_id: 5, appointment_type_id: 1, note: "Diagnosis: Mild UTI", appointment_date: "2018-04-15"},
+  {doctor_id: 1, patient_id: 4, appointment_type_id: 2, note: "Vaccine: Chickenpox", appointment_date: "2017-06-02"},
+  {doctor_id: 9, patient_id: 3, appointment_type_id: 2, note: "Vaccine: Flu", appointment_date: "2018-03-27"},
+  {doctor_id: 3, patient_id: 2, appointment_type_id: 1, note: "Diagnosis: Nausea and dizziness due to low blood pressure", appointment_date: "2018-05-08"}
 ]
 
 appointments.each {|appointment| Appointment. create(appointment)}
