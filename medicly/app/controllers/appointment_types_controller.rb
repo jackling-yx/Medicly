aabb798890228a33d861aa22af75b04405348447
ja9-look <janineluk95@gmail.com>
2018-12-30 17:28:23 +0000

    updated appointment type controller

--- medicly/app/controllers/appointment_types_controller.rb
+++ medicly/app/controllers/appointment_types_controller.rb
@@ -1,26 +1,45 @@
 class AppointmentTypesController < ApplicationController
   before_action :find_appointment_type, only: [:show, :edit, :update, :destroy]
 
-    def index
-    end
+  def index
+    @appointment_types = AppointmentType.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-    end
+  def new
+    @appointment_type = AppointmentType.new
+  end
 
-    def create
+  def create
+    @appointment_type = AppointmentType.new(appointment_type_params)
+    if @appointment_type.valid?
+      @appointment_type.save
+      redirect_to appointment_type_path(@appointment_type)
+    else
+      flash[:errors] = @appointment_type.errors.full_messages
+      render :new
     end
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
+  def update
+    if @appointment_type.valid?
+      @appointment_type.update(appointment_type_params)
+      redirect_to appointment_type_path(@appointment_type)
+    else
+      flash[:errors] = @appointment_type.errors.full_messages
+      render :edit
     end
+  end
 
-    def destroy
-    end
+  def destroy
+    @appointment_type.destroy
+    redirect_to appointment_types_path
+  end
 
   private
 
